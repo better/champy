a6362857631ca5d5335d26c33c045b537d8127ee
Erik Bernhardsson <mail@erikbern.com>
2017-10-12 07:21:28 -0400

    initial stuff

---
+++ lib.py
@@ -0,0 +1,119 @@
+class Expression:
+    def __init__(self, expr, constant=0):
+        self._expr = expr
+        self._constant = constant
+
+    def __mul__(self, rhs):
+        assert type(rhs) in [float, int]
+        return Expression(tuple((k*rhs, v) for k, v in self._expr))
+
+    def __rmul__(self, lhs):
+        return self * lhs
+
+    def __add__(self, rhs):
+        if type(rhs) in [float, int]:
+            return Expression(self._expr, self._constant + rhs)
+        else:
+            assert isinstance(rhs, Expression)
+            # TODO: should dedupe variables here
+            return Expression(self._expr + rhs._expr, self._constant + rhs._constant)
+
+    def __radd__(self, lhs):
+        return self * lhs
+
+    def __neg__(self):
+        return self * -1
+
+    def __sub__(self, rhs):
+        return self + (-rhs)
+
+    def __rsub__(self, lhs):
+        return lhs + (-self)
+
+    def __abs__(self):
+        return Problem.abs(self)
+    
+    def __lt__(self, rhs):
+        return Polytope(((-self - rhs, '>'),))
+
+    def __le__(self, rhs):
+        return Polytope(((-self - rhs, '>='),))
+
+    def __gt__(self, rhs):
+        return Polytope(((self - rhs, '>'),))
+
+    def __ge__(self, rhs):
+        return Polytope(((self - rhs, '>='),))
+
+    def __eq__(self, rhs):
+        return Polytope(((self - rhs, '=='),))
+
+    def __str__(self):
+        return 'Expression(%s+%.2f)' % (''.join('%+.2f%s' % (k, v) for k, v in self._expr), self._constant)
+
+
+class Variable(Expression):
+    def __init__(self, name=None, lo=None, hi=None, cat=None):
+        self._name = name
+        self._expr = ((1, self),)
+        self._constant = 0
+        self._lo = lo
+        self._hi = hi
+        self._cat = cat
+
+    def __str__(self):
+        return self._name
+
+
+class Polytope:
+    def __init__(self, constraints):
+        self._constraints = tuple(constraints)
+
+    @staticmethod
+    def all(polytopes):
+        return Polytope(sum((c._constraints for c in polytopes), tuple()))
+
+    @staticmethod
+    def any(polytopes, big_M=999999):
+        # This one is the most interesting
+        magic = [Variable('magic', 0, 1, 'binary') for p in polytopes]
+        new_constraints = []
+        for polytope, m in zip(polytopes, magic):
+            for expr, op in polytope._constraints:
+                new_constraints.append((expr + m * big_M, op))
+        return Polytope(new_constraints) & \
+            (sum(magic) <= len(polytopes)-1)
+
+    def __and__(self, rhs):
+        return Polytope.all((self, rhs))
+
+    def __or__(self, rhs):
+        return Polytope.any((self, rhs))
+    
+    def __str__(self):
+        return 'Polytope(%s)' % ', '.join('%s %s 0' % (str(expr), op) for expr, op in self._constraints)
+
+
+class Problem:
+    def __init__(self, polytope, objective):
+        self._polytope = polytope
+        self._objective = objective
+
+    @staticmethod
+    def abs(m):
+        z_neg = Variable('z_neg', lo=0, hi=None)
+        z_pos = Variable('z_pos', lo=0, hi=None)
+        print(m + z_neg + z_pos == 0)
+        return Problem(m + z_neg + z_pos == 0, z_neg + z_pos)
+
+    def __str__(self):
+        return 'Problem(min %s s.t. %s)' % (self._objective, self._polytope)
+
+x = Variable('x')
+y = Variable('y')
+z = Variable('z')
+c1 = 3 * (x - y*2) > 7 * x
+c2 = z < y
+print(c1 & c2)
+print(c1 | c2)
+print(abs(x - y))
